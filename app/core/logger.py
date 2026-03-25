3cefd729b4453666868c14770463b9cfa9309ba7
wangxun <1013296326@qq.com>
2026-03-25 16:34:45 +0800

    log

--- app/core/logger.py
+++ app/core/logger.py
@@ -12,5 +12,16 @@ def setup_logger():
         diagnose=False,  # 关闭变量值
         backtrace=False,  # 关闭完整堆栈跟踪
         colorize=False,
-        enqueue=True  # 启用异步日志处理
+        enqueue=True,  # 启用异步日志处理
+        filter=lambda record: record['level'].no < 30,  # 低于 WARNING(30)
+    )
+    # WARNING 及以上
+    logger.add(
+        sink=sys.stderr,  # 输出到标准错误流
+        format='{time:YYYY-MM-DD HH:mm:ss.SSS} - {level} - {message}',  # 日志格式
+        level='WARNING',
+        diagnose=False,  # 关闭变量值
+        backtrace=False,  # 关闭完整堆栈跟踪
+        colorize=False,
+        enqueue=True,  # 启用异步日志处理
     )
