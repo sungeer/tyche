1918a473becf626220c63fb6f5c2e35b603feca6
wangxun <1013296326@qq.com>
2026-04-01 14:09:50 +0800

    add

--- src/core/lifespan.py
+++ src/core/lifespan.py
@@ -1,11 +1,13 @@
 from contextlib import asynccontextmanager
 
 from src.core.logger import setup_logger
-from src.core import db
+from src.core import db, executor
 
 
 @asynccontextmanager
 async def lifespan(app):
     setup_logger()
     yield
+    executor.db_threadpool.shutdown(wait=True)
+    executor.bio_threadpool.shutdown(wait=True)
     db.engine.dispose()
