379774ad01ce5dbfd108f1db1f9099f5156dcb66
wangxun <1013296326@qq.com>
2026-04-03 17:23:24 +0800

    add

--- src/core/db.py
+++ src/core/db.py
@@ -5,8 +5,8 @@ from src.core.config import settings
 engine = create_engine(
     settings.db_url,
     echo=False,  # 不打印SQL语句
-    pool_size=20,  # 空闲连接 上限
-    max_overflow=20,  # 高峰额外最多再开 10 条
+    pool_size=5,  # 空闲连接 上限
+    max_overflow=7,  # 高峰额外最多再开 10 条
     pool_timeout=30,  # 取连接等待 30s 失败就报错
     pool_recycle=1800,  # 回收重连
     pool_pre_ping=True,  # 避免拿到失效连接
