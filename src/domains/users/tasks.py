1d628caa65d3c36f573645a4e8b4cec946fe44b9
Want Sun <1013296326@qq.com>
2026-04-05 11:04:34 +0800

    add

--- src/domains/users/tasks.py
+++ src/domains/users/tasks.py
@@ -4,10 +4,12 @@ from huey import crontab
 
 from src.core.queue import huey
 from src.core.logger import logger
+from src.core.context import run_id_var, new_run_id
 
 
 @huey.periodic_task(crontab(minute='*/1'))
 def health_check():
     """每5分钟执行一次健康检查"""
-    logger.info(f"[Periodic] 健康检查 - {datetime.now()}")
+    run_id_var.set(new_run_id())
+    logger.info(f'[Periodic] 健康检查 - {datetime.now()}')
     return {'status': 'healthy'}
