900c68bca81c47fb4ced6c3c8f861f024eb0bdee
Want Sun <1013296326@qq.com>
2026-04-07 07:13:33 +0800

    add

--- src/core/config.py
+++ src/core/config.py
@@ -1,16 +1,6 @@
 import os
-import sys
 from pathlib import Path
 from urllib.parse import quote_plus
-
-is_debug = os.getenv('DEBUG') == '1'
-
-is_win = sys.platform.startswith('win')
-
-if is_debug or is_win:
-    config_name = 'dev'
-else:
-    config_name = 'prod'
 
 
 class BaseConfig:
@@ -19,12 +9,12 @@ class BaseConfig:
     log_path = base_dir / 'logs/app.log'
 
     jwt_algorithm = 'HS256'  # 加密算法
-    jwt_access_token_expire_minutes = 30  # token 有效期 30s
-    jwt_refresh_token_expire_days = 7
+    jwt_access_token_expire_minutes = 30  # 访问令牌有效期 30分钟
+    jwt_refresh_token_expire_days = 7  # 刷新令牌有效期 7天
 
 
 class DevConfig(BaseConfig):
-    with_debug = 1
+    is_debug = 1
 
     origins = ['http://127.0.0.1:8080']  # cors 允许的来源 前端应用使用的端口
 
@@ -37,7 +27,7 @@ class DevConfig(BaseConfig):
 
 
 class ProdConfig(BaseConfig):
-    with_debug = 0
+    is_debug = 0
 
     origins = ['http://127.0.0.1:8080']  # cors 允许的来源 前端应用使用的端口
 
@@ -52,5 +42,9 @@ config_map = {
     'dev': DevConfig,
     'prod': ProdConfig
 }
+
+is_debug = os.getenv('DEBUG') == '1'
+
+config_name = 'dev' if is_debug else 'prod'
 
 settings = config_map.get(config_name, ProdConfig)
